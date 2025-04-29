b4161bb14d10ac131b7704c69212dabc5802574f
Shehjaar Manwati <shehjaarmanwati24@gmail.com>
2025-04-30 01:08:00 +0530

    cron.js added

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import express from "express";
 import "dotenv/config";
 import authRoutes from "./routes/authRoutes.js";
 import bookRoutes from "./routes/bookRoutes.js";
+import job from "../lib/cron.js";
 
 import {connectDB} from "./lib/db.js";
 
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+job.start();
 
 app.use(express.json());
 
